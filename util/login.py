599e24f0e0fdd14f63263ebe8471fb0b44ae9ba2
BBfat <1056871944@qq.com>
2019-03-28 21:47:21 +0800

    更新login模块

--- util/login.py
+++ util/login.py
@@ -51,4 +51,4 @@ def getUserInfo(code,access_token):
     userInfo['email'] = tempInfo["d"]["email"]
     userInfo['mobile'] = tempInfo["d"]["mobile"]
     userInfo['sex'] = tempInfo["d"]["sex"]
-    return tempInfo
+    return userInfo
