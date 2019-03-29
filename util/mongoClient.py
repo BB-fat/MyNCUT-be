9f3e876659b072c8d2bc21f49c1ce5acaaadf5b4
Llym <1448189829@qq.com>
2019-03-29 14:26:21 +0800

    数据库新增部分函数 增加两个常量变量

---
+++ util/mongoClient.py
@@ -0,0 +1,41 @@
+from pymongo import MongoClient
+from setting import *
+class mongoClient ():
+    def __init__(self):
+        """
+        数据库初始化，初始化userInfo集合
+        """
+        self.client =MongoClient(DATABASEIP,DATABASEPORT)
+
+
+    def newUser(self,openid,userInfo):
+        """
+        插入用户信息
+        :param openid:
+        :param userInfo:
+        :return: none
+        userData 数据库
+        user 集合（openid 字段、userInfo 字段）
+        """
+        userdata={"openid":openid,"userInfo":userInfo}
+        self.client.userData["user"].insert_one(userdata)
+    def getUserInfo(self,openid):
+        """
+        获取用户全部信息
+        :param openid:
+        :return: userInfo
+        """
+        getUserInfoResult=self.client.userData["user"].find_one({"openid":openid})
+        return getUserInfoResult
+    def getPublicInfo(self):
+        """
+        获取新闻信息
+        :return: {"indexBannner":,"indexNotice":}
+        """
+        bannner=[]
+        notice=[]
+        for eachBanner in self.client.publicInfo["indexBanner"].find():
+            bannner.append(eachBanner)
+        for eachNotice in self.client.publicIndo["indexNotice"].find():
+            notice.append(eachNotice)
+        return {"indexBanner":bannner,"indexNotice":notice}
