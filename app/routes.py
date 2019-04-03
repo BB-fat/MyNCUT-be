f345b25902710fedf52df0d578f462e96a4d1a24
BBfat <1056871944@qq.com>
2019-04-03 16:04:39 +0800

    update

--- app/routes.py
+++ app/routes.py
@@ -48,7 +48,7 @@ def getUserInfoByOpenid():
     :return:
     '''
     openid=request.args.get('openid')
-    return json.dumps(mongoClient().getUserInfo(openid))
+    return json.dumps(mongoClient().getUserInfo(openid)['userInfo'])
 
 @app.route('/publicinfo')
 def getBannerAndNotice():
