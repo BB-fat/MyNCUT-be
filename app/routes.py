e69ebe9c6f66ec92cc550a1e6d23f472c2925969
BB-fat <1056871944@qq.com>
2019-04-11 22:16:21 +0800

    debug

--- app/routes.py
+++ app/routes.py
@@ -35,6 +35,10 @@ def getUserInfoByCode():
     code=request.args.get('code')
     openid=getOpenid(code)
     userInfo=app.DB.getUserInfo(openid)
+    userInfoTemp={
+        'openid':openid,
+        'userInfo':userInfo
+    }
     return json.dumps(userInfo)
 
 @app.route("/login/openid")
