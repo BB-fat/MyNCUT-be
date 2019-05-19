c9a9e7050ebe83b465c24948dbe91405abcd4d22
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 01:49:35 +0800

    开发点滴校园

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import cx_Oracle
 # dsn=cx_Oracle.makedsn("10.100.0.1",1521,"ORCL")
 # connect=cx_Oracle.connect("MYNUCT","AQU1m0Lhp8",dsn)
-dsn=cx_Oracle.makedsn("10.100.1.248",1521,"epps")
+dsn=cx_Oracle.makedsn("10.100.1.248",1521,service_name="epps")
 connect=cx_Oracle.connect("C##MYNUCT","c8SYjM05x7U",dsn)
 c=connect.cursor()
 sql='''
-select XH,KCS,ZXF,ZGFKC,ZGF,ZDFKC,ZDF from C##MYNUCT.DDXY_STU_COURSE  where XH='17152010921'
+select XH,KCS,ZXF,ZGFKC,ZGF,ZDFKC,ZDF from DDXY_STU_COURSE  where XH='17152010921'
 '''
 res=c.execute(sql)
 print (res.fetchall())
