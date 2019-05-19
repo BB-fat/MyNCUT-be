1a834109433091b14a56f739d838c38368053647
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 01:27:48 +0800

    开发点滴校园

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ dsn=cx_Oracle.makedsn("10.100.1.248",1521,"epps")
 connect=cx_Oracle.connect("C##MYNUCT","c8SYjM05x7U",dsn)
 c=connect.cursor()
 sql='''
-SELECT owner FROM all_tables WHERE table_name = 'DDXY_STU_BASICINFO';
+SELECT * from  *.DDXY_STU_BASICINFO where XH='17152010921'
 '''
 res=c.execute(sql)
 print (res.fetchone())
