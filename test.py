350d1143277f8abc0da0c0823b37450911f32c91
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 01:07:56 +0800

    开发点滴校园

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ dsn=cx_Oracle.makedsn("10.100.1.248",1521,"epps")
 connect=cx_Oracle.connect("C##MYNUCT","c8SYjM05x7U",dsn)
 c=connect.cursor()
 sql='''
-select XH,XM,XB,SYD,CC,XY,ZY,BJ from DDXY_STU_COURSE
+select round(SUM(C.LL)/1024) as TotalGib from NCUT_CSRDLLSJ  C where  C.YHM = '17152010921'
 '''
 res=c.execute(sql)
 print (res.fetchone())
