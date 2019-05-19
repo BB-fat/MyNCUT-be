7e769c484f98393c14d7a974fe860cec184e4ed9
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 00:48:38 +0800

    update

---
+++ test.py
@@ -0,0 +1,13 @@
+import cx_Oracle
+dsn=cx_Oracle.makedsn("10.100.1.248",1521,"epps")
+connect=cx_Oracle.connect("C##MYNUCT","c8SYjM05x7U",dsn)
+c=connect.cursor()
+sql='''
+select DISTINCT KCMC,JSMC from DDXY_STU_FIRSTCOURSE   F1
+where XH='15104050234' AND SKXQ=(select MIN(SKXQ) from DDXY_STU_FIRSTCOURSE where XH=F1.XH) 
+AND SKDY=(select MIN(SKDY) from DDXY_STU_FIRSTCOURSE where XH=F1.XH and (SKXQ=(select MIN(SKXQ) from DDXY_STU_FIRSTCOURSE where XH=F1.XH) ))
+'''
+res=c.execute(sql)
+print (res)
+c.close()                                                 #关闭cursor
+conn.close()  
