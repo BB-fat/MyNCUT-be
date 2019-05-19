7fb33e889af9767fd0a82c5b23f1e1b8cbabce38
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 02:58:23 +0800

    开发点滴校园

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import cx_Oracle
 # dsn=cx_Oracle.makedsn("10.100.0.1",1521,"ORCL")
 # connect=cx_Oracle.connect("MYNUCT","AQU1m0Lhp8",dsn)
-dsn=cx_Oracle.makedsn("10.100.1.248",1521,"epps")
+dsn=cx_Oracle.makedsn("10.100.1.248",1521,service_name="epps")
 connect=cx_Oracle.connect("C##MYNUCT","c8SYjM05x7U",dsn)
 c=connect.cursor()
 sql='''
