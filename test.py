5a9049e74249000517f8ee34edb369cf78219f48
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 01:12:44 +0800

    开发点滴校园

--- test.py
+++ test.py
@@ -2,7 +2,10 @@ import cx_Oracle
 dsn=cx_Oracle.makedsn("10.100.0.1",1521,"ORCL")
 connect=cx_Oracle.connect("MYNUCT","AQU1m0Lhp8",dsn)
 c=connect.cursor()
-sql="select round(SUM(C.LL)/1024) as TotalGib from NCUT_CSRDLLSJ  C where  C.YHM = '17152010921'"
+sql='''
+select to_char(ZXSJ,'HH24') as HOUR,ROUND(SUM(C.LL)/1024) as G from NCUT_CSRDLLSJ C where  C.YHM='17152010921'  GROUP BY to_char(ZXSJ,'HH24')
+ORDER BY to_char(ZXSJ,'HH24') ASC
+'''
 res=c.execute(sql)
 print (res.fetchone())
 c.close()
