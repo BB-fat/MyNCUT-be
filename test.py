e163c33912ed3c973d0543a243d7f2184481dd07
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 19:17:09 +0800

    开发点滴校园

--- test.py
+++ test.py
@@ -4,13 +4,16 @@ os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 
 import cx_Oracle
 
-# print (cx_Oracle.clientversion())
-conn = cx_Oracle.connect("C##MYNUCT", "c8SYjM05x7U", "10.100.1.248/orcl")
-# print (conn.version)
+conn = cx_Oracle.connect("MYNUCT", "AQU1m0Lhp8", "10.100.0.1/orcl")
 
-sql = """
-SELECT * from C##NCUTDATA.DDXY_STU_BASICINFO where XH='17152010921'
-"""
+# conn = cx_Oracle.connect("C##MYNUCT", "c8SYjM05x7U", "10.100.1.248/orcl")
+
+# sql = """
+# SELECT * from C##NCUTDATA.DDXY_STU_BASICINFO where XH='17152010921'
+# """
+sql='''
+select round(SUM(C.LL)/1024) as TotalGib from NCUTDATA.NCUT_CSRDLLSJ  C where  C.YHM = '000120'
+'''
 
 #sql = "select * from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where rownum <=5"
 
