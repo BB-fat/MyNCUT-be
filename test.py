c0389db9e2293fd86cb4f26e7ff3f86f45cbb96d
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 18:30:33 +0800

    开发点滴校园

--- test.py
+++ test.py
@@ -1,15 +1,22 @@
+# -*- coding: utf-8 -*-
+import os
+os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
+
 import cx_Oracle
-# dsn=cx_Oracle.makedsn("10.100.0.1",1521,"ORCL")
-# connect=cx_Oracle.connect("MYNUCT","AQU1m0Lhp8",dsn)
-dsn=cx_Oracle.makedsn("10.100.1.248",1521,"epps")
-connect=cx_Oracle.connect("C##MYNUCT","c8SYjM05x7U",dsn)
-c=connect.cursor()
-sql='''
-select DISTINCT KCMC,JSMC from C##MYNUCT.DDXY_STU_FIRSTCOURSE   F1
-where XH='15104050234' AND SKXQ=(select MIN(SKXQ) from DDXY_STU_FIRSTCOURSE where XH=F1.XH)
-AND SKDY=(select MIN(SKDY) from DDXY_STU_FIRSTCOURSE where XH=F1.XH and (SKXQ=(select MIN(SKXQ) from DDXY_STU_FIRSTCOURSE where XH=F1.XH) ))
-'''
-res=c.execute(sql)
-print (res)
-c.close()
-connect.close()
+
+print (cx_Oracle.clientversion())
+conn = cx_Oracle.connect("C##MYNUCT", "c8SYjM05x7U", "10.100.1.248/orcl")
+print (conn.version)
+
+sql = """select DISTINCT KCMC,JSMC from C##NCUTDATA.DDXY_STU_FIRSTCOURSE   F1
+where XH='15104050234' AND SKXQ=(select MIN(SKXQ) from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where XH=F1.XH) 
+AND SKDY=(select MIN(SKDY) from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where XH=F1.XH and (SKXQ=(select MIN(SKXQ) from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where XH=F1.XH) ))
+"""
+
+#sql = "select * from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where rownum <=5"
+
+cursor = conn.cursor()
+
+for each in cursor.execute(sql):
+  print(each[0])
+  print(each[1])
