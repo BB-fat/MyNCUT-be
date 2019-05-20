d2682ecc725eca3f648416e217e55424fb7cb2c1
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 18:58:27 +0800

    开发点滴校园

--- test.py
+++ test.py
@@ -5,13 +5,11 @@ os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 import cx_Oracle
 
 # print (cx_Oracle.clientversion())
-conn = cx_Oracle.connect("C##MYNUCT", "c8SYjM05x7U", "10.100.1.248/epps")
+conn = cx_Oracle.connect("C##MYNUCT", "c8SYjM05x7U", "10.100.1.248/orcl")
 # print (conn.version)
 
 sql = """
-select DISTINCT KCMC,JSMC from C##NCUTDATA.DDXY_STU_FIRSTCOURSE   F1
-where XH='16104050207' AND SKXQ=(select MIN(SKXQ) from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where XH=F1.XH) 
-AND SKDY=(select MIN(SKDY) from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where XH=F1.XH and (SKXQ=(select MIN(SKXQ) from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where XH=F1.XH) ))
+SELECT * from C##MYNUCT.DDXY_STU_BASICINFO where XH='17152010921'
 """
 
 #sql = "select * from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where rownum <=5"
