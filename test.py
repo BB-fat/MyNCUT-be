34f8cb098c63ea8fa615d59e77f10e3e6bd9b453
鞠欣诚 <bbfat3047@qq.com>
2019-05-20 18:33:47 +0800

    开发点滴校园

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 
 import cx_Oracle
 
-print (cx_Oracle.clientversion())
+# print (cx_Oracle.clientversion())
 conn = cx_Oracle.connect("C##MYNUCT", "c8SYjM05x7U", "10.100.1.248/orcl")
-print (conn.version)
+# print (conn.version)
 
 sql = """
-select XH,KCS,ZXF,ZGFKC,ZGF,ZDFKC,ZDF from DDXY_STU_COURSE  where XH='17152010921'
+select XH,KCS,ZXF,ZGFKC,ZGF,ZDFKC,ZDF from C##MYNUCT.DDXY_STU_COURSE  where XH='17152010921'
 """
 
 #sql = "select * from C##NCUTDATA.DDXY_STU_FIRSTCOURSE where rownum <=5"
