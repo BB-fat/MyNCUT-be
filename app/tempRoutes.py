c63c75d3dac1c42992365c7ff7c799534b2d8e80
鞠欣诚 <bbfat3047@qq.com>
2019-05-22 16:14:37 +0800

    开发点滴校园

--- app/tempRoutes.py
+++ app/tempRoutes.py
@@ -39,6 +39,7 @@ def schoolLife():
     data=SchoolLife(userInfo['userid']).getData()
     data['count']=app.DB.SL_countPlus(userInfo['userid'])
     data['sex']=userInfo['sex']
+    print(data['sex'])
     return render_template("2019SchoolLife.html", data=data)
 
 @app.route("/schoollifeauth")
