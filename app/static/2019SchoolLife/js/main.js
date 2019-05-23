79e5acdfe26640ee26cee9d2d7bf2c1777481a01
鞠欣诚 <bbfat3047@qq.com>
2019-05-24 06:17:25 +0800

    开发点滴校园

--- app/static/2019SchoolLife/js/main.js
+++ app/static/2019SchoolLife/js/main.js
@@ -104,8 +104,8 @@ function makePic(img_url, text) {
         ctx.font = "35px Arial";
         var y = 150
         for (var i = 0; i < text.length; i++) {
-            ctx.fillText(text[i], 100, y);
-            y += 100
+            ctx.fillText(text[i], 90, y);
+            y += 80
         }
         var url = myCanvas.toDataURL("image/png", 1)
         document.getElementById("share-img").src = url
