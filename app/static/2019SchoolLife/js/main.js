d3054b5e6891df96ffda7c7abc5de5bb71f9d9ed
鞠欣诚 <bbfat3047@qq.com>
2019-05-24 00:15:31 +0800

    开发点滴校园

--- app/static/2019SchoolLife/js/main.js
+++ app/static/2019SchoolLife/js/main.js
@@ -24,55 +24,12 @@ var mySwiper = new Swiper('.swiper-container', {
                 .set('opacity', '1')
                 .duration("5s")
                 .end()
-            if(this.activeIndex==6){
+            if (this.activeIndex == 6) {
                 chart.restart();
             } else if (this.activeIndex == 9) {
-                 $('.bar').each(function(i, elem){
-    drawBar(elem);
-  });
-  
-  $('.measure').each(function(i, elem){
-    drawMeasure(elem);
-  });
-  
-  $('a.redraw').click(function(e){
-    e.preventDefault();
-    $('.bar').each(function(i, elem){
-      randomiseBar(elem);
-    });
-    $('.measure').each(function(i, elem){
-      randomiseMeasure(elem);
-    });
-  
-  });
-  
-  function drawBar(bar) {
-    var percentage = $(bar).data('percentage');
-    if(percentage > 100){
-      percentage = 100;
-    }
-    $(bar).animate({'width': percentage + '%'}, 'slow');
-  }
-  
-  function randomiseBar(bar) {
-    var width =  Math.floor(Math.random() * (100 - 20 + 1)) + 20;
-    $(bar).animate({'width': width + '%'}, 'slow');
-    $(bar).attr('data-percentage', width);
-  }
-  
-  function drawMeasure(measure) {
-    var percentage = $(measure).data('percentage');
-    if(percentage > 100){
-      percentage = 100;
-    }
-    $(measure).animate({'width': percentage + '%'}, 'slow');
-  }
-  
-  function randomiseMeasure(measure) {
-    var width =  Math.floor(Math.random() * (100 - 20 + 1)) + 20;
-    $(measure).animate({'width': width + '%'}, 'slow');
-    $(measure).attr('data-percentage', width);
-  }
+                $('.bar').each(function (i, elem) {
+                    drawBar(elem);
+                });
             }
         },
     }
