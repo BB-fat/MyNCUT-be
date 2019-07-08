8aad3b2da0a366f1be7b76c1130d415f9bed40c2
鞠欣诚 <bbfat3047@qq.com>
2019-07-08 13:45:12 +0800

    下线点滴校园

--- app/tempRoutes.py
+++ app/tempRoutes.py
@@ -1,8 +1,6 @@
 from app import app
 from flask import render_template,send_file, request
-from werkzeug.utils import secure_filename
 from app.modules.login import *
-from app.modules.SL_2019 import *
 import app.modules.feedback as fb
 
 @app.route('/kSyoZqkJwk.txt')
@@ -12,10 +10,6 @@ def wxAuth():
     :return:
     '''
     return send_file('kSyoZqkJwk.txt')
-
-@app.route("/test")
-def test():
-    return render_template("2019SchoolLife.html")
 
 @app.route("/getauth")
 def getAuth():
@@ -34,68 +28,9 @@ def storeData():
         app.DB.setIndexNotice(tempInfo)
     return "success"
 
-@app.route("/schoollife")
-def schoolLife():
-    openid = request.args.get('openid')
-    userInfo = app.DB.getUserInfo(openid)
-    data=SchoolLife(userInfo['userid']).getData()
-    data['name']=userInfo['userInfo']['name']
-    data['count']=app.DB.SL_countPlus(userInfo['userid'])
-    data['sex']=userInfo['userInfo']['sex']
-    data['msgs']=app.DB.SL_takeMsg(30)
-    data['same_name_school'],data['same_name_college'],data['city']=app.DB.SL_same_city(userInfo['userid'])
-    data['book']=library(userInfo['userid'])
-    data['userid']=userInfo['userid']
-    data['wenyi']=app.DB.SL_wenyi_url()
-    return render_template("2019SchoolLife.html", data=data)
-    # return render_template("test.html",data=json.dumps(data))
-
-@app.route("/schoollifeauth")
-def schoolLifeAuth():
-    '''
-    通过点滴校园认证
-    :return:
-    '''
-    access_token = getAccess_token()
-    openid = request.args.get('state')
-    code = request.args.get('code')
-    userInfo = getUserInfo(code, access_token)
-    app.DB.newUser(openid)
-    app.DB.setUserInfo(openid, userInfo)
-    userInfo = app.DB.getUserInfo(openid)
-    data = SchoolLife(userInfo['userid']).getData()
-    data['name'] = userInfo['userInfo']['name']
-    data['count'] = app.DB.SL_countPlus(userInfo['userid'])
-    data['sex'] = userInfo['userInfo']['sex']
-    data['msgs'] = app.DB.SL_takeMsg(30)
-    data['same_name_school'], data['same_name_college'], data['city'] = app.DB.SL_same_city(userInfo['userid'])
-    data['book'] = library(userInfo['userid'])
-    data['userid'] = userInfo['userid']
-    data['wenyi']=app.DB.SL_wenyi_url()
-    return render_template("2019SchoolLife.html", data=data)
-
-@app.route("/schoollifemsg")
-def schoolLifeMsg():
-    msg=request.args.get("msg")
-    userid=request.args.get("userid")
-    userInfo=app.DB.getUserInfo(userid=userid)
-    app.DB.SL_leaveMsg({
-        "msg":msg,
-        "userid":userid,
-        "img":userInfo['userInfo']['avatar']
-    })
-    return "success"
-
 @app.route("/wenyi")
 def wenyi():
     app.DB.SL_count_wenyi()
-    return 'success'
-
-
-@app.route("/uploadpic",methods=['GET','POST'])
-def upload():
-    f=request.files['wenyipic']
-    f.save('/home/myncut/wenyipic/'+secure_filename(f.filename))
     return 'success'
 
 @app.route("/getfeedback")
