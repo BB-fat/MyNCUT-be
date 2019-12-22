5eb688d0db92767702c162f5de2d79f36cddf541
BBfat <bbfat3047@qq.com>
2019-12-22 11:56:41 +0800

    重构前留档 有支付相关接口

--- runserver.py
+++ runserver.py
@@ -1,5 +1,8 @@
 from app import app
+import sys
 from setting import *
 
 if __name__=="__main__":
+    if sys.argv[1]=="-d":
+        DEBUG=True
     app.run(host="127.0.0.1",port=8080,debug=DEBUG)
