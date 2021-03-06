20f9da16fd1888b9cfbf3ca6329a45ee1d7c416e
alexju <alexju@tencent.com>
2021-03-06 15:38:24 +0800

    update

--- setting.py
+++ setting.py
@@ -1,29 +1,29 @@
 # DATABASEIP = '10.211.55.5'
 DATABASEIPDEV = '10.100.1.222'
 DATABASEIP = '10.102.1.35'
-DB_USER = "myncut"
-DB_PASSWD = "myNCUT@wx315wdbf"
+DB_USER = ""
+DB_PASSWD = ""
 
-yxAPPID = '31b1e992583074382'
+yxAPPID = ''
 
-yxAPPSECRET = 'f5e030ee903dbc29c6e76375253d1ee6'
+yxAPPSECRET = ''
 
-DATABASEPORT=27017
+DATABASEPORT = 27017
 
-wxAPPID="wx12bef97a98001f0b"
+wxAPPID = ""
 
-wxAPPSECRET="dbd5ecc6424d1ce668c93e7f98120c53"
+wxAPPSECRET = ""
 
-payAPPID="f6d910b62057eff25"
-payAPPSECRET="ba468bc53567e916953de03d111b8b08"
+payAPPID = ""
+payAPPSECRET = ""
 
-VALIDTIME=300
+VALIDTIME = 300
 
-EMAIL_SERVER='smtp.qq.com'
-EMAIL_PORT='465'
-EMAIL_USERNAME='1056871944@qq.com'
-EMAIL_PASSWD='snjpnnztwsnxbbbf'
-EMAIL_TO_ADDRESS=['1056871944@qq.com']
+EMAIL_SERVER = ''
+EMAIL_PORT = ''
+EMAIL_USERNAME = ''
+EMAIL_PASSWD = ''
+EMAIL_TO_ADDRESS = []
 
 FILE_TYPES = {
     'pdf': 'application/pdf',
@@ -34,8 +34,8 @@ FILE_TYPES = {
     'zip': 'application/zip',
     'xls': 'application/x-xls',
     'xlsx': 'application/x-xls',
-    'avi':'video/avi',
-    'jpg':'image/jpeg',
-    'mp4':'video/mpeg4',
-    'rar':'application/octet-stream'
+    'avi': 'video/avi',
+    'jpg': 'image/jpeg',
+    'mp4': 'video/mpeg4',
+    'rar': 'application/octet-stream'
 }
